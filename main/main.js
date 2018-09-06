bd768fc7cd7c3d95587e2d194cc56d58712a36aa
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-06 21:08:16 +0800

    refactor: use one reduce method to count A and B

--- main/main.js
+++ main/main.js
@@ -1,21 +1,16 @@
-// Write your cade below:
-
 module.exports = function main(answer, input) {
 
-    let countA = input.split('').reduce((acc, cur, index) => {
-        if (index === answer.indexOf(cur)) {
-            acc++;
+    let count = input.split('').reduce((acc, cur, index) => {
+        if (answer.includes(cur)) {
+            if (index === answer.indexOf(cur)) {
+                acc.A++;
+            } else {
+                acc.B++;
+            }
         }
         return acc;
-    }, 0);
+    }, {A: 0, B: 0});
 
-    let countB = input.split('').reduce((acc, cur, index) => {
-        if (answer.includes(cur) && index !== answer.indexOf(cur)) {
-            acc++;
-        }
-        return acc;
-    }, 0);
-
-    return `${countA}A${countB}B`;
+    return `${count.A}A${count.B}B`;
 };
 
