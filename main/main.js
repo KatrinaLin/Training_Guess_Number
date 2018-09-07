b05531daadbbbaf76d7800f6405c81d8dc759eae
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-07 10:18:42 +0800

    refactor if else branch

--- main/main.js
+++ main/main.js
@@ -2,27 +2,10 @@ module.exports = function compare(answer, input) {
 
     let count = answer.split('').reduce((acc, cur, index) => {
         if (input.includes(cur)) {
-            if (cur === input.charAt(index)) {
-                acc.A++;
-            } else {
-                acc.B++;
-            }
+            cur === input.charAt(index) ? acc.A++ : acc.B++;
         }
         return acc;
     }, {A: 0, B: 0});
 
     return `${count.A}A${count.B}B`;
 };
-
-function generateAnswer() {
-    let answer = [];
-
-    while (answer.length < 4) {
-        let random = Math.floor(Math.random() * 10);
-        if (answer.includes(random)) {
-            continue;
-        }
-        answer.push(random);
-    }
-    return answer.join();
-}
